5af08320c969c514fa0d08d81913a91d82626789
David E. Perez Negron R <david@neetsec.com>
2023-02-27 12:57:09 -0600

    Update scripts

--- scripts/CastVotes.ts
+++ scripts/CastVotes.ts
@@ -4,16 +4,6 @@ import * as dotenv from 'dotenv';
 import { Signer } from "ethers";
 dotenv.config();
 
-const PROPOSALS = ["Proposal 1", "Proposal 2", "Proposal 3"];
-
-function convertStringArrayToBytes32(array: string[]) {
-  const bytes32Array = [];
-  for (let index = 0; index < array.length; index++) {
-    bytes32Array.push(ethers.utils.formatBytes32String(array[index]));
-  }
-  return bytes32Array;
-}
-
 async function main() {
   
   const provider = new ethers.providers.InfuraProvider(
@@ -31,16 +21,15 @@ async function main() {
 
   const ballotFactory = await new Ballot__factory(signer);
   const ballotContract = await ballotFactory.attach(
-    "0xaf5bd48C8dd8F733697e148F952c62868A701b71"
+    "0x906D466Db831B2C2F375Ee05A2E8AEfeC4696723"
   );
-  //await ballotContract.deployTransaction.wait();
   console.log(
     `attached contract address is ${ballotContract.address}` 
   );
-  const giveRightsToVote = await ballotContract.giveRightToVote("0xDDd93CEC5843f471Eb2b8B2886b2Be32555B5209")
-  console.log(giveRightsToVote)
-  const vote = await ballotContract.vote("1")
-  console.log(vote)
+  //const giveRightsToVote1 = await ballotContract.giveRightToVote("0xDDd93CEC5843f471Eb2b8B2886b2Be32555B5209")
+  //console.log(giveRightsToVote1)
+  const giveRightsToVote2 = await ballotContract.giveRightToVote("0xD64258a33E7AC0294a9fdE8e4C9A76674bD33A23")
+  console.log(giveRightsToVote2)
 }
 
 main().catch((error) => {
--- scripts/DelegatingVotes.ts
+++ scripts/DelegatingVotes.ts
@@ -12,7 +12,7 @@ async function main() {
   );
 
   console.log({ provider });
-  const pkey = process.env.PRIVATE_KEY;
+  const pkey = process.env.PRIVATE_KEY_VOTE;
   console.log({ pkey });
   const lastBlock = await provider.getBlock("latest");
   console.log({ lastBlock });
@@ -21,12 +21,12 @@ async function main() {
 
   const ballotFactory = await new Ballot__factory(signer);
   const ballotContract = await ballotFactory.attach(
-    "0xaf5bd48C8dd8F733697e148F952c62868A701b71"
+    "0x906D466Db831B2C2F375Ee05A2E8AEfeC4696723"
   );
   console.log(
     `attached contract address is ${ballotContract.address}` 
   );
-  const delegate = await ballotContract.delegate("0xD64258a33E7AC0294a9fdE8e4C9A76674bD33A23")
+  const delegate = await ballotContract.delegate("0xDDd93CEC5843f471Eb2b8B2886b2Be32555B5209")
   console.log(delegate)
 }
 
